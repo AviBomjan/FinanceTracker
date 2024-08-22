2d9548894e59d1e31dcc211d05eabdd2ee508846
avibo <avibo@AVI>
2024-08-22 13:09:22 +1000

    Add project files.

---
+++ FinanceTracker/Models/FinanceTrackerDBContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FinanceTracker.Models
+{
+    public class FinanceTrackerDBContext : DbContext
+    {
+        public DbSet<Expense> Expenses { get; set; }
+        public FinanceTrackerDBContext(DbContextOptions<FinanceTrackerDBContext> options) : base(options) 
+        {
+
+        }
+    }
+}
